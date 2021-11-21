59a5141bcc188de71e0c70d3c9ebd3e308d0e807
wwyyww <yso00302@gmail.com>
2021-11-21 23:20:09 +0900

    Update RidingActivity.kt Check upload GPS data

--- app/src/main/java/com/example/moti/RidingActivity.kt
+++ app/src/main/java/com/example/moti/RidingActivity.kt
@@ -13,15 +13,23 @@ import android.provider.Settings
 import android.util.Log
 import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.skt.Tmap.TMapGpsManager
 import com.skt.Tmap.TMapGpsManager.GPS_PROVIDER
 import com.skt.Tmap.TMapGpsManager.NETWORK_PROVIDER
+import com.skt.Tmap.TMapPoint
+import com.skt.Tmap.TMapPolyLine
 import com.skt.Tmap.TMapView
 import kotlinx.android.synthetic.main.activity_riding.*
-
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import java.util.*
 
 class RidingActivity:AppCompatActivity(), TMapGpsManager.onLocationChangedCallback  {
 
@@ -29,22 +37,57 @@ class RidingActivity:AppCompatActivity(), TMapGpsManager.onLocationChangedCallba
 
     // T Map GPS
     var tMapGPS: TMapGpsManager? = null
+    lateinit var polyline : TMapPolyLine
 
     companion object {
         const val PERMISSION_REQUEST_CODE = 1001
     }
 
+    //db용
+    private var auth : FirebaseAuth? = null
+    private lateinit var database: DatabaseReference
+    private val CurrentUser = FirebaseAuth.getInstance().currentUser
+    val uid = CurrentUser?.uid
+    private lateinit var pushRef:DatabaseReference
+
+
+    var mCalendar = Calendar.getInstance()
+    lateinit var todayDate:String
+
+    var courseCount=0
+
     @RequiresApi(Build.VERSION_CODES.M)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_riding)
+        riding_course_textview.bringToFront()
+        riding_gps_textview.bringToFront()
+        riding_button.bringToFront()
 
+
+        auth = FirebaseAuth.getInstance()
+        database = Firebase.database.reference
+
+        todayDate = (mCalendar.get(Calendar.YEAR)).toString() + "/" + (mCalendar.get(Calendar.MONTH) + 1).toString() +
+                "/" + (mCalendar.get(Calendar.DAY_OF_MONTH)).toString()
+
+        pushRef=database.child("user/$uid/course/date").child(todayDate).push()
+
+
+        //action bar 숨기기
+        var actionBar: ActionBar?
+        actionBar=supportActionBar
+        actionBar?.hide()
+
+        //tmap 세팅
         tmapview = TMapView(this)
         tmapview!!.setSKTMapApiKey("l7xx3a42dd9f094c468e969018fba936e361")
+        tmapview!!.setIconVisibility(true)
+        tmapview!!.setCompassMode(true)
+        tmapview!!.setTrackingMode(true)
         tmap_layout.addView(tmapview)
 
-
-
+        //위치 권한 확인
         if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION)
             == PackageManager.PERMISSION_GRANTED) {
             Toast.makeText(this, "권한 승인", Toast.LENGTH_SHORT).show()
@@ -61,17 +104,34 @@ class RidingActivity:AppCompatActivity(), TMapGpsManager.onLocationChangedCallba
         tMapGPS!!.minDistance= 10F
         tMapGPS!!.provider=NETWORK_PROVIDER
 
-//        tMapGPS!!.provider=GPS_PROVIDER
         tMapGPS!!.OpenGps()
+        polyline= TMapPolyLine()
+
+        riding_button.setOnClickListener{
+            pushRef.child("$courseCount/lat").setValue("0")
+            pushRef.child("$courseCount/lng").setValue("1")
+            courseCount+=1
+
+        }
+
+
 
 
     }
 
 
 
+
+
     override fun onLocationChange(location: Location) {
         tmapview!!.setLocationPoint(location.longitude, location.latitude)
         tmapview!!.setCenterPoint(location.longitude, location.latitude)
+
+        var lat=location.latitude
+        var long=location.longitude
+        var point=TMapPoint(lat, long)
+        polyline.addLinePoint(point)
+        tmapview!!.addTMapPolyLine("line", polyline)
         Log.i("gps", "check gps change")
 
 
@@ -124,5 +184,6 @@ class RidingActivity:AppCompatActivity(), TMapGpsManager.onLocationChangedCallba
         val dialog = builder.create()
         dialog.show()
     }
+
 
 }
