71279a227400004f053cccb68e986386853c4df9
jeongminhui99 <jeongminhee99@naver.com>
2021-11-24 20:44:12 +0900

    Update kakaoapi2

--- app/src/main/java/com/example/moti/KaKaoAPI.kt
+++ app/src/main/java/com/example/moti/KaKaoAPI.kt
@@ -21,7 +21,10 @@ interface KaKaoAPI_category {
         @Header("Authorization") key: String,     // 카카오 API 인증키 [필수]
         //@Query("query") query: String             // 검색을 원하는 질의어 [필수]
         // 매개변수 추가 가능
-        @Query("category_group_code") category: String // 카테고리 코드 [필수]
+        @Query("category_group_code") category: String, // 카테고리 코드 [필수]
+        @Query("x") x: String, //  longitude
+        @Query("y") y: String, // latitude
+        @Query("radius") radius: Int // 중심 좌표부터의 반경거리
 
     ): Call<ResultCategoryKeyword>    // 받아온 정보가 ResultSearchKeyword 클래스의 구조로 담김
 }
