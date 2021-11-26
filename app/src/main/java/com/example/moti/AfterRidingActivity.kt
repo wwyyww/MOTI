4ba02fcea7c857f9334fc7855deba88d42c6f150
wwyyww <yso00302@gmail.com>
2021-11-26 15:39:32 +0900

    Update select Hashtag

--- app/src/main/java/com/example/moti/AfterRidingActivity.kt
+++ app/src/main/java/com/example/moti/AfterRidingActivity.kt
@@ -1,10 +1,12 @@
 package com.example.moti
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_after_riding.*
+import kotlinx.android.synthetic.main.activity_riding.*
 
 
 class AfterRidingActivity: AppCompatActivity() {
@@ -24,6 +26,15 @@ class AfterRidingActivity: AppCompatActivity() {
         after_time_txtview.text=intent.getStringExtra("time")
         pushKey= intent.getStringExtra("pushKey")!!
         Log.d("intent", "check pushKey ${pushKey}")
+
+
+        after_next_button.setOnClickListener {
+            val intent = Intent(this, SharingActivity::class.java).apply {
+//                putExtra("time", riding_timer_textview.text)
+                putExtra("pushKey", pushKey)
+
+            }.run {startActivity(this) }
+        }
 
 
     }
